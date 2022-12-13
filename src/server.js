2e5c61196f1b2c8c8c4dc94b5c8a5a5aab86040a
changhwan_park <ch.park@kaist.ac.kr>
2022-12-13 23:58:31 +0900

    feat: get, middleware practice

--- src/server.js
+++ src/server.js
@@ -3,4 +3,18 @@ import express from "express";
 const app = express();
 const PORT = 4000;
 
+const middleHome = (req, res, next) => {
+  console.log(`Going to ${req.url}`);
+  next();
+};
+const handleHome = (req, res) => {
+  return res.end("<h1>Hello</h1>");
+};
+const handleLogin = (req, res) => {
+  return res.send("Login Here");
+};
+
+app.get("/", middleHome, handleHome);
+app.get("/login", handleLogin);
+
 app.listen(PORT, () => console.log(`Port on ${PORT}`));
