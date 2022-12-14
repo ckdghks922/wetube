4552e9ee01628341ac3da7f6fabbb2a66c5de395
changhwan_park <ch.park@kaist.ac.kr>
2022-12-15 00:05:23 +0900

    feat: router 1차 적용

--- src/server.js
+++ src/server.js
@@ -1,21 +1,17 @@
 import express from 'express';
 import morgan from 'morgan';
+import globalRouter from './routers/globalRouter';
+import userRouter from './routers/userRouter';
+import videoRouter from './routers/videoRouter';
 
 const app = express();
 const PORT = 4000;
 
 const logger = morgan('dev');
-
-const handleHome = (req, res) => {
-  return res.end('<h1>Hello</h1>');
-};
-const handleLogin = (req, res) => {
-  return res.send('Login Here');
-};
-
 app.use(logger);
 
-app.get('/', handleHome);
-app.get('/login', handleLogin);
+app.use('/', globalRouter);
+app.use('/users', userRouter);
+app.use('/videos', videoRouter);
 
 app.listen(PORT, () => console.log(`Port on ${PORT}`));
