76bdffcb1f9464b21fd443c1eec58947077be6cb
changhwan_park <ch.park@kaist.ac.kr>
2022-12-15 23:09:17 +0900

    feat: prototype pug 추가

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,11 +1,11 @@
-export const handleHome = (req, res) => res.send('Home');
+export const handleHome = (req, res) => res.render('home');
 
 export const handleSearch = (req, res) => res.send('Search');
 
 export const handleUpload = (req, res) => res.send('Upload Video');
 
-export const handleSee = (req, res) => res.send('See Video');
+export const handleSee = (req, res) => res.render('see');
 
-export const handleEdit = (req, res) => res.send('Edit Video');
+export const handleEdit = (req, res) => res.render('edit');
 
 export const handleDelete = (req, res) => res.send('Delete Video');
--- src/server.js
+++ src/server.js
@@ -8,10 +8,13 @@ const app = express();
 const PORT = 4000;
 
 const logger = morgan('dev');
+
+app.set('view engine', 'pug');
+app.set('views', process.cwd() + '/src/views');
 app.use(logger);
 
 app.use('/', globalRouter);
 app.use('/users', userRouter);
 app.use('/videos', videoRouter);
 
-app.listen(PORT, () => console.log(`Port on ${PORT}`));
+app.listen(PORT, () => console.log(`Port listening on ${PORT}`));
