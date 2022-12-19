c7006644cc369a648aea260a8c40ca9b3acd33e6
changhwan_park <ch.park@kaist.ac.kr>
2022-12-20 00:07:21 +0900

    feat: add mixins

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,5 +1,33 @@
-export const handleHome = (req, res) =>
-  res.render('home', { pageTitle: 'Home' });
+const videos = [
+  {
+    title: 'Video 1',
+    ratings: 3,
+    comements: 2,
+    createdAt: '10 minutes ago',
+    views: 12,
+    id: 1,
+  },
+  {
+    title: 'Video 2',
+    ratings: 4,
+    comements: 12,
+    createdAt: '15 minutes ago',
+    views: 19,
+    id: 2,
+  },
+  {
+    title: 'Video 3',
+    ratings: 5,
+    comements: 5,
+    createdAt: '20 minutes ago',
+    views: 22,
+    id: 3,
+  },
+];
+
+export const handleHome = (req, res) => {
+  res.render('home', { pageTitle: 'Home', videos });
+};
 
 export const handleSearch = (req, res) => res.send('Search');
 
