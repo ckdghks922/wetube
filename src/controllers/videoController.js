ba98ed8fcc136af2fa0d291f8b08d054e86ea8e7
changhwan_park <ch.park@kaist.ac.kr>
2022-12-21 01:47:04 +0900

    feat: add upload template

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -2,7 +2,7 @@ const videos = [
   {
     title: 'Video 1',
     ratings: 3,
-    comements: 2,
+    comments: 2,
     createdAt: '10 minutes ago',
     views: 1,
     id: 1,
@@ -10,7 +10,7 @@ const videos = [
   {
     title: 'Video 2',
     ratings: 4,
-    comements: 12,
+    comments: 12,
     createdAt: '15 minutes ago',
     views: 19,
     id: 2,
@@ -18,7 +18,7 @@ const videos = [
   {
     title: 'Video 3',
     ratings: 5,
-    comements: 5,
+    comments: 5,
     createdAt: '20 minutes ago',
     views: 22,
     id: 3,
@@ -35,10 +35,23 @@ export const handleWatch = (req, res) => {
   return res.render('watch', { pageTitle: `Watch ${video.title}`, video });
 };
 
+export const handleGetUpload = (req, res) => {
+  return res.render('upload', { pageTitle: 'Upload' });
+};
+
+export const handlePostUpload = (req, res) => {
+  return res.redirect('/');
+};
+
 export const handleGetEdit = (req, res) => {
   const { id } = req.params;
   const video = videos[id - 1];
   return res.render('edit', { pageTitle: `Edit ${video.title}`, video });
 };
 
-export const handlePostEdit = (req, res) => {};
+export const handlePostEdit = (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+  videos[id - 1].title = title;
+  return res.redirect(`/videos/${id}`);
+};
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,14 +1,16 @@
 import express from 'express';
 import {
   handleWatch,
+  handleGetUpload,
+  handlePostUpload,
   handleGetEdit,
   handlePostEdit,
 } from '../controllers/videoController';
 
 const videoRouter = express.Router();
 
+videoRouter.route('/upload').get(handleGetUpload).post(handlePostUpload);
 videoRouter.get('/:id', handleWatch);
-videoRouter.get('/:id/edit', handleGetEdit);
-videoRouter.post('/:id/edit', handlePostEdit);
+videoRouter.route('/:id/edit').get(handleGetEdit).post(handlePostEdit);
 
 export default videoRouter;
